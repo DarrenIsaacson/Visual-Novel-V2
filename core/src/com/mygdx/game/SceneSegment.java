eb856c1482563b1a259473b5822f66c09c3059f0
darre <darren@gmail.com>
2019-12-08 20:31:08 -0600

    Initial commit

---
+++ core/src/com/mygdx/game/SceneSegment.java
@@ -0,0 +1,39 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+public class  SceneSegment
+{
+    private Actor actor;
+    private Action action;
+    
+    public SceneSegment(Actor a1, Action a2)
+    {
+        actor = a1;
+        action = a2;
+    }
+    
+    public void start()
+    {
+        actor.clearActions();
+        actor.addAction(action);
+    }
+    
+    public boolean isFinished()
+    {
+        return (actor.getActions().size == 0);
+    }
+    
+    /** 
+     *  End this segment early
+     */
+    public void finish()
+    {
+        // simulate 100000 seconds elapsed time to complete in-progress action
+        if ( actor.hasActions() )
+            actor.getActions().first().act(100000);
+        
+        actor.clearActions();
+    }
+}
